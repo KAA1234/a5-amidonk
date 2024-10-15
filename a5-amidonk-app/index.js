82469da90d19c54996560633b83eb25afa4311a9
KAA1234 <94076913+KAA1234@users.noreply.github.com>
2024-10-15 23:09:35 +0000

    completed the express exploration from a5

--- a5-amidonk-app/index.js
+++ a5-amidonk-app/index.js
@@ -1,6 +1,17 @@
-'use strict';
 
-const http = require('node:http');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("This app is using express.");
+});
+
+app.listen(PORT, () =>  {
+  console.log(`Server listening on port ${PORT}...`);
+});
+
+/* const http = require('node:http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -13,4 +24,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-}); 
+});  */
