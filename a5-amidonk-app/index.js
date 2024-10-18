de2d47e7cc6066ca3bee1fa9f0a7d5634176b928
KAA1234 <94076913+KAA1234@users.noreply.github.com>
2024-10-18 23:44:54 +0000

    Assignment A5 Step 3 complete

--- a5-amidonk-app/index.js
+++ a5-amidonk-app/index.js
@@ -1,7 +1,15 @@
+'use strict';
+
 
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({
+  extended:true
+}));
+
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
   res.send("This app is using express.");
@@ -11,17 +19,3 @@ app.listen(PORT, () =>  {
   console.log(`Server listening on port ${PORT}...`);
 });
 
-/* const http = require('node:http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});  */
